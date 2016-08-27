74a228e544f25478b6b25ffee5a19d48a3983d7e
nickangtc <nickangtc@gmail.com>
2016-08-27 16:53:19 +0800

    Wrote 9 new function skeletons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 console.log('javascript working');
+
+$(document).ready(function () {
+
+  var TIMER_ID = "";
+  var TIME_LEFT = 99;
+  var GAME_OVER = false;
+
+  var IMAGES = [img1, img2, img3, img4, img5, img6, img7];
+  var img1 = {
+    // cssImgLeft: "img1a",
+    // cssImgRight: "img1b",
+    answerIndex: [10, 20, 30, 40, 50]
+  };
+  var img2 = {
+    answerIndex: [10, 20, 30, 40, 50]
+  };
+
+  // ---- Main control-flow function ----
+  // executes when any pixel is clicked.
+  function playTurn (choice) {
+
+
+    function currentQuestion () {}
+
+    function isGameOver () {}
+
+    function whoWon () {}
+
+    function isRight (pixel) {}
+
+  };
+
+  // 'start' / 'stop' timer
+  // 'add time for time extension
+  function timer (option) {
+
+  }
+
+  function restart () {}
+
+  function popUpMsg (msg) {}
+
+  function displayMsg (msg) {}
+
+});
