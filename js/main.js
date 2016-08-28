f095750896f1ee5977b9efe12ef6efba79f37e99
nickangtc <nickangtc@gmail.com>
2016-08-28 16:30:05 +0800

    Wrote and tested onclick function and dittoClick function

--- js/main.js
+++ js/main.js
@@ -8,14 +8,15 @@ $(document).ready(function () {
 
   var IMAGES = [img1, img2];
   var img1 = {
-    // cssImgLeft: "img1a",
-    // cssImgRight: "img1b",
     answerIndex: [10, 20, 30, 40, 50]
   };
   var img2 = {
     answerIndex: [10, 20, 30, 40, 50]
   };
 
+  // !--- GAME LOGIC ---! //
+
+  // Generate click listeners for game 'pixels'.
   for (var i = 1; i <= 150; i++) {
     $('#pix-l-' + i).on('click', playTurn);
     $('#pix-r-' + i).on('click', playTurn);
@@ -25,10 +26,12 @@ $(document).ready(function () {
   // executes when any pixel is clicked.
   function playTurn (choice) {
     var element = choice.target;
-    console.log(element.id + ' was clicked!');
-    $("#" + element.id).addClass("selected-circle");
+    $('#' + element.id).addClass('selected-circle');
+    console.log("clicked on: " + element.id);
 
-    function currentQuestion () {}
+    dittoClick(element);  // execute ONLY if choice is right
+
+    function currentChallenge () {}
 
     function isGameOver () {}
 
@@ -36,7 +39,19 @@ $(document).ready(function () {
 
     function isRight (pixel) {}
 
-  };
+    // Duplicates clicks on one panel on the other.
+    function dittoClick (element) {
+      var leftOrRight = element.id.charAt(4);
+      var toClick = "";
+      if (leftOrRight === 'r') {
+        toClick = element.id.replace('r', 'l');
+      } else {
+        toClick = element.id.replace('l', 'r');
+      }
+      $("#" + toClick).addClass("selected-circle");
+    }
+
+  }
 
   // 'start' / 'stop' timer
   // 'add time for time extension
