31b42076626dcd63de9aca6cda73e3aed6d49405
nickangtc <nickangtc@gmail.com>
2016-09-01 10:34:21 +0800

    Added answer arrays for all 3 images in game

--- js/main.js
+++ js/main.js
@@ -31,12 +31,40 @@ $(document).ready(function () {
     found: [] // will receive arrays of found areas from logFound()
   };
   var img2 = {
-    ansCoords: [10, 20, 30, 40, 50],
-    cssClass: 'img2'
+    ansCoords: [  // right answer LOCUS
+      [368, 392],  // [x, y] zeroed coordinates
+      [283, 395],
+      [34, 379],
+      [346, 429],
+      [134, 192]
+    ],
+    ansArea: [  // right answer AREA
+      [10, 10], // [x-radius, y-radius]
+      [32, 32],
+      [75, 75],
+      [18, 18],
+      [55, 30]
+    ],
+    cssClass: 'img2',
+    found: [] // will receive arrays of found areas from logFound()
   };
   var img3 = {
-    ansCoords: [10, 20, 30, 40, 50],
-    cssClass: 'img3'
+    ansCoords: [  // right answer LOCUS
+      [185, 467],  // [x, y] zeroed coordinates
+      [78, 462],
+      [303, 189],
+      [253, 357],
+      [297, 574]
+    ],
+    ansArea: [  // right answer AREA
+      [25, 25], // [x-radius, y-radius]
+      [30, 30],
+      [50, 50],
+      [100, 60],
+      [50, 20]
+    ],
+    cssClass: 'img3',
+    found: [] // will receive arrays of found areas from logFound()
   };
 
   // Stores all previously the above img objects.
